fb4c5de6f19f5e5392749520f31aa57548454939
Ruslan8999 <rponomarenko89@yandex.ru>
2022-11-29 14:55:25 +0300

    feat: add_controllers branch of Sprint 13

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface ItemRepository {
     Collection<Item> getAllItems();
+
     Item getById(long id);
+
     Item saveItem(Item item);
+
     Item editItem(Item item);
+
     boolean checkItems(long id);
 }
--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface UserRepository {
     Collection<User> findAll();
+
     User findById(long id);
+
     User create(User user);
+
     User update(long id, User user);
+
     void remove(long id);
 }
